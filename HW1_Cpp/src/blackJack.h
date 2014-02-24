27f74bb1edec5a20e9fe7040fc13d24843dbd10e
ws23 <linyutsu23@gmail.com>
2014-02-24 16:26:15 +0800

    practice OOP

---
+++ HW1_Cpp/src/blackJack.h
@@ -0,0 +1,52 @@
+class poker{
+	private:
+		int card[52];
+		int numCards;
+	public:
+		poker();
+		int getCard(int);
+		void shuffle();
+		int dealCard();
+		string showCard(int);
+};
+
+class player{
+	private:
+		int bet;
+		int nbet;
+		int card[5];
+		int point;
+	public:
+		player();
+		void setCard(int);
+		void setBet(int);
+		void setNBet(int);
+		void setPoint(int);
+		int getCard();
+		int getBet();
+		int getNBet();
+		int getPoint();
+		string showCard();
+		void selectOpt();
+};
+
+class computer:public player{
+	public:
+		computer();
+		void selectOpt();
+		string showCard();
+};
+
+class blackJack{
+	private:
+		player p;
+		computer c;
+		poker ini;
+	public:
+		void startGame();
+		void deal();
+		int judge();
+		int judgeWin();
+		void processBet(int, int);
+		void showHelp();
+};
