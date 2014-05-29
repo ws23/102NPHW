bc35b6abe01e33979be9f7d67dde6947591158d2
ws23 <linyutsu23@gmail.com>
2014-05-29 14:21:49 +0800

    update #SmallProgram 05/20

--- course/4_socket/410121021_140520/client.c
+++ course/4_socket/410121021_140520/client.c
@@ -1,5 +1,10 @@
-#include "unp.h"
+#include <netinet/in.h>                                                 
+#include <sys/socket.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#define MAXLINE 1024
 
 /* Input a value, send to server, show the result from server */
 
@@ -12,34 +17,37 @@ int main(int argc, char *argv[]){
 		printf("usage: %s <IP Address>\n", argv[0]); 
 		exit(0); 
 	}
-
-	if((sockfd = socket(AF_INET, SOCK_STREAM, 0))<0){
-		printf("socket error\n"); 
-		exit(1); 
-	}
-
-	bzero(&servaddr, sizeof(servaddr)); 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_port = htons(13); 
-	if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr)<=0){
-		printf("inet_pton error for %s\n", argv[1]); 
-		exit(1); 
-	}	
-	if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))<0){
-		printf("connect error\n"); 
-		exit(1); 
-	}
-
-	while((n = read(sockfd, recvline, MAXLINE))>0){
-		recvline[n] = 0; 
-		if(fputs(recvline, stdout)==EOF){
-			printf("fputs error\n"); 
+		if((sockfd = socket(AF_INET, SOCK_STREAM, 0))<0){
+			printf("socket error\n"); 
 			exit(1); 
 		}
-	}
-	if(n<0){
-		printf("read error\n"); 
-	}
 
+		bzero(&servaddr, sizeof(servaddr)); 
+		servaddr.sin_family = AF_INET; 
+		servaddr.sin_port = htons(1243); 
+		if(inet_pton(AF_INET, argv[1], &servaddr.sin_addr)<=0){
+			printf("inet_pton error for %s\n", argv[1]); 
+			exit(1); 
+		}	
+		if(connect(sockfd, (struct sockaddr*)&servaddr, sizeof(servaddr))<0){
+			printf("connect error\n"); 
+			exit(1); 
+		}
+
+
+	while(1){
+		printf("Input a value to calculate(<0 to exit): \n> "); 
+		scanf("%d", &n);
+		sprintf(recvline, "%d", htonl(n)); 
+		write(sockfd, recvline, sizeof(recvline)); 
+		printf("send %d\n", n);
+		if(n<0)
+			break;
+		read(sockfd, recvline, sizeof(recvline)); 
+		n = atoi(recvline); 
+		n = ntohl(n); 
+		printf("%d\n", n); 
+	}
+	close(sockfd); 
 	exit(0); 
 }
--- course/4_socket/410121021_140520/server.c
+++ course/4_socket/410121021_140520/server.c
@@ -1,33 +1,61 @@
-#include "unp.h"
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>                                                 
+#include <sys/socket.h>
+#define MAXLINE 1024
 
 /*Calculate the fib(n)*/
 
-int main(int argc, char *argv[]){
-	int listenfd, connfd; 
-	struct sockaddr_in servaddr; 
-	char buff[MAXLINE]; 
-	time_t ticks; 
+int fib(int n){
+	static i = 2, fib[100] = {0, 1}; 
+	if(i==2)
+		for(;i<100;i++)
+			fib[i] = fib[i-1] + fib[i-2]; 
+	return fib[n]; 
+}
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0); 
+int main(int argc, char *argv[]){
+	int listenfd, connfd, len, n; 
+	struct sockaddr_in servaddr, clntaddr; 
+	char buff[MAXLINE]; 
+
+	if((listenfd = socket(AF_INET, SOCK_STREAM, 0))<0){
+		printf("socket error\n"); 
+		exit(1); 
+	}
+
 	bzero(&servaddr, sizeof(servaddr)); 
 	servaddr.sin_family = AF_INET; 
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-	servaddr.sin_port = htons(13); 
-	
-	bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)); 
+	servaddr.sin_port = htons(1243); 
+	if(bind(listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr))<0){
+		printf("bind error\n"); 
+		exit(1); 
+	} 
+
 	listen(listenfd, 128); 
 
 	while(1){
-		connfd = accept(listenfd, NULL, NULL); 
-
-		ticks = time(NULL);
-		snprintf(buff, sizeof(buff), "%.24ss\r\n", ctime(&ticks)); 
-		
-		write(connfd, buff, strlen(buff)); 
-
+		len = sizeof(clntaddr); 	
+		connfd = accept(listenfd, (struct sockaddr*)&clntaddr, &len); 
+		printf("accept success\n"); 
+		while(1){
+			read(connfd, buff, sizeof(buff));
+			n = atoi(buff); 
+			n = ntohl(n);
+			printf("get %d\n", n);
+			if(n<0)
+				break;
+			n = fib(n); 
+			sprintf(buff, "%d", htonl(n)); 
+			write(connfd, buff, sizeof(buff));
+			printf("send %d\n", n);
+			read(connfd, buff, sizeof(buff)); 
+		}
 		close(connfd); 
 	}
-
-
+	close(listenfd); 
+	return 0; 
 }
